0d62521d186afd3863c815298c48949feae84abe
Maverick00093 <bansidavariya093@gmail.com>
2025-07-28 13:26:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use('/admin', adminRoutes);
 app.use('/', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 0241;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
